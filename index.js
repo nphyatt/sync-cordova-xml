e8629c0eda9621b6404f8936ba0dd3573e236d36
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2018-09-14 18:32:57 -0500

    rework the script with xml-js

--- index.js
+++ index.js
@@ -1,75 +1,90 @@
-var xml = require('xmldom')
+var xml = require('xml-js')
 var assert = require('nanoassert')
 var parseAuthor = require('parse-author')
+
+function getNodeByName(name, elements) {
+  for (element of elements) {
+    if(element.name === name) {
+      return element;
+    }
+  }
+}
+
+function errorAndExit(errMsg) {
+  console.error(errMsg);
+  process.exit(-1);
+}
+
+function upsertTextNode(parent, name, text) {
+  node = getNodeByName(name, parent.elements);
+  if (!node) {
+    parent.elements.push({
+      type: 'element',
+      name: name,
+      elements: [{type: 'text', text: text}]
+    })
+    return;
+  }
+  for (el of node.elements) {
+    if (el.type === 'text') {
+      el.text = text;
+    }
+  }
+}
+
 module.exports = function (jsonObj, xmlStr, mode) {
   assert(jsonObj != null)
   assert(typeof xmlStr === 'string')
 
-  var xmlDoc = new xml.DOMParser().parseFromString(xmlStr, 'text/xml')
+  var xmlDoc = new xml.xml2js(xmlStr)
 
-  if (jsonObj.version) xmlDoc.documentElement.setAttribute('version', jsonObj.version)
+  switch (mode) {
+    case 'plugin':
+      plugin = getNodeByName('plugin', xmlDoc.elements);
+      plugin.attributes.version = jsonObj.version || errorAndExit('No version found in package.json');
+      plugin.attributes.id = trimScope(jsonObj.name) || errorAndExit('No package name found in package.json');
+      name = getNodeByName('name', plugin.elements)
+      name.elements[0].text = jsonObj.name;
+      if (jsonObj.description) {
+        upsertTextNode(plugin, 'description', jsonObj.description);
+      }
+      if (jsonObj.license) {
+        upsertTextNode(plugin, 'license', jsonObj.license);
+      }
+      if (Array.isArray(jsonObj.keywords)) {
+        upsertTextNode(plugin, 'keywords', jsonObj.keywords.join());
+      }
+      if (jsonObj.repository) {
+        if (typeof jsonObj.repository === 'string') {
+          upsertTextNode(plugin, 'repo', jsonObj.repository);
+        } else if (jsonObj.repository.url) {
+          upsertTextNode(plugin, 'repo', jsonObj.repository.url);
+        }
+      }
+      if (jsonObj.bugs) {
+        if (jsonObj.bugs.url) {
+          upsertTextNode(plugin, 'issue', jsonObj.bugs.url);
+        }
+      }
+      break;
+    case 'config':
+      widget = getNodeByName('widget', xmlDoc.elements);
+      widget.attributes.version = jsonObj.version || errorAndExit('No version found in package.json');
 
-  if (jsonObj.name) {
-    if (mode === 'plugin') xmlDoc.documentElement.setAttribute('id', trimScope(jsonObj.name))
-    upsertNode(xmlDoc.documentElement, 'name', {}, jsonObj.name)
+      // displayName is a cordova convention
+      if (jsonObj.displayName) {
+        upsertTextNode(widget, 'name', jsonObj.displayName);
+      }
+      if (jsonObj.description) {
+        upsertTextNode(widget, 'description', jsonObj.description);
+      }
+      break;
+    default:
+      break;
   }
 
-  // displayName is a cordova convention
-  if (jsonObj.displayName && mode === 'config') {
-    upsertNode(xmlDoc.documentElement, 'name', {}, jsonObj.displayName)
-  }
-
-  if (jsonObj.description) {
-    upsertNode(xmlDoc.documentElement, 'description', {}, jsonObj.description)
-  }
-
-  if (mode === 'plugin') {
-    if (jsonObj.license) upsertNode(xmlDoc.documentElement, 'license', {}, jsonObj.license)
-    if (Array.isArray(jsonObj.keywords)) upsertNode(xmlDoc.documentElement, 'keywords', {}, jsonObj.keywords.join())
-
-    if (jsonObj.repository) {
-      if (typeof jsonObj.repository === 'string') upsertNode(xmlDoc.documentElement, 'repo', {}, jsonObj.repository)
-      if (jsonObj.repository.url) upsertNode(xmlDoc.documentElement, 'repo', {}, jsonObj.repository.url)
-    }
-
-    if (jsonObj.bugs) {
-      if (jsonObj.bugs.url) upsertNode(xmlDoc.documentElement, 'issue', {}, jsonObj.bugs.url)
-    }
-
-    if (jsonObj.author) {
-      if (typeof jsonObj.author === 'string') jsonObj.author = parseAuthor(jsonObj.author)
-      upsertNode(xmlDoc.documentElement, 'author', {
-        email: jsonObj.author.email,
-        href: jsonObj.author.url
-      }, jsonObj.author.name)
-    }
-  }
-
-  return new xml.XMLSerializer().serializeToString(xmlDoc)
-
-  function upsertNode(parent, name, attrs, content) {
-    var nodes = parent.getElementsByTagName(name)
-    var node = nodes.length > 0 ? nodes[0] : xmlDoc.createElement(name)
-
-    Object.keys(attrs).forEach(function (key) {
-      if (attrs[key] == null) return
-      node.setAttribute(key, attrs[key])
-    })
-
-    for (var i = 0; i < node.childNodes.length; i++) {
-      node.removeChild(node.childNodes.item(i))
-    }
-
-    if (typeof content === 'string') node.appendChild(xmlDoc.createTextNode(content))
-
-    if (nodes.length === 0) {
-      var ws = xmlDoc.createTextNode(parent.firstChild.data) // get indentation
-      var nl = xmlDoc.createTextNode('\n') // Create trailing new line
-      parent.replaceChild(ws, parent.lastChild) // Replace existing new line (last child)
-      parent.appendChild(node) // actually append node
-      parent.appendChild(nl) // Insert new line again
-    }
-  }
+  stringXml = xml.js2xml(xmlDoc, {spaces: 4, indentCdata: true});
+  return stringXml;
 }
 
 
